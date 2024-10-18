3cc93423aa84f9063195b5390bf013d9dc9350bd
seghiaranass <anass.segh97@gmail.com>
2024-10-18 10:39:43 +0100

    Making Components Reusable with Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,52 @@
-const reactDescriptions = ['Fundamental', 'Crucial','Core']
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
-function genRandomInit(max){
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInit(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[genRandomInit(2)]} React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[genRandomInit(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConecpt(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}  />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
 function App() {
   return (
     <div>
-        <Header />
+      <Header />
       <main>
+
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConecpt title="Components" description="The core UI building block" image={componentsImg}/>
+            <CoreConecpt title="Components" description="The core UI building block" image={componentsImg}/>
+            <CoreConecpt title="Components" description="The core UI building block" image={componentsImg}/>
+            <CoreConecpt title="Components" description="The core UI building block" image={componentsImg}/>
+
+          </ul>
+        </section>
+
         <h2>Time to get started!</h2>
       </main>
     </div>
