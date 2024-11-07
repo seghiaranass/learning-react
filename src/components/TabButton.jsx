3935db39f85b8d48b4c853b59caff551cf816133
seghiaranass <anass.segh97@gmail.com>
2024-11-07 17:21:05 +0100

    Passing Functions as Values To Props

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,5 +1,9 @@
-export default function TabButton({children}) {
-  return <>
-    <li><button>{children}</button></li>
-  </>;
+export default function TabButton({ children, onSelect }) {
+
+
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
 }
