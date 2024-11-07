178ea259c5d7079628707b049e003f41acd0b06d
seghiaranass <anass.segh97@gmail.com>
2024-11-07 18:15:45 +0100

    Deriving & Outputting Data Based on State

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConecpt from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
-
+import { EXAMPLES } from './data';
 
 
 function App() {
-  const [selectedTopic,setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic,setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton)
@@ -34,13 +34,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=>{handleSelect("Components")}}>Components</TabButton>
-            <TabButton onSelect={()=>{handleSelect("JSX")}}>JSX</TabButton>
-            <TabButton onSelect={()=>{handleSelect("Props")}}>Props</TabButton>
-            <TabButton onSelect={()=>{handleSelect("State")}}>State</TabButton>
+            <TabButton onSelect={()=>{handleSelect("components")}}>Components</TabButton>
+            <TabButton onSelect={()=>{handleSelect("jsx")}}>JSX</TabButton>
+            <TabButton onSelect={()=>{handleSelect("props")}}>Props</TabButton>
+            <TabButton onSelect={()=>{handleSelect("state")}}>State</TabButton>
           </menu>
 
-          {selectedTopic}
+          <div id="tab-content">
+
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
 
 
 
