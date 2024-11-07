d71ac2bf8e23c72ad0f4a1696d19f260ec704549
seghiaranass <anass.segh97@gmail.com>
2024-11-07 16:39:17 +0100

    Best Practice: Stroing Components in Files & Using a Good Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-import reactImg from "./assets/react-core-concepts.png";
+
 import componentsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInit(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[genRandomInit(2)]} React concepts you will need for
-        almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConecpt(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title}  />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header";
+import CoreConecpt from "./components/CoreConcept";
 
 function App() {
   return (
